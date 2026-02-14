7d5ccad4c2eec66f36c6331960d4a9cdf967c147
viniciussavelaseibt-lgtm <viniciussavelaseibt@gmail.com>
2026-02-14 14:11:56 -0300

    Implement average score calculation for athletes
    Added a function to calculate and display the average scores of athletes.

--- notas-atletas.js
+++ notas-atletas.js
@@ -1,1 +1,45 @@
-Commit changes
+let atletas = [
+ {
+   nome: "Cesar Abascal",
+   notas: [10, 9.34, 8.42, 10, 7.88]
+ },
+ {
+   nome: "Fernando Puntel",
+   notas:  [8, 10, 10, 7, 9.33]
+ },
+ {
+   nome: "Daiane Jelinsky",
+   notas: [7, 10, 9.5, 9.5, 8]
+ },
+ {
+   nome: "Bruno Castro",
+   notas: [10, 10, 10, 9, 9.5]
+ }
+];
+
+function calcularMedia(atletas) {
+
+    for (let i = 0; i < atletas.length; i++) {
+
+        let nome = atletas[i].nome;
+        let notas = atletas[i].notas;
+
+        notas.sort((a, b) => a - b);
+
+        let notasValidas = notas.slice(1, 4);
+
+        let soma = 0;
+        notasValidas.forEach(function(nota) {
+            soma += nota;
+        });
+
+        let media = soma / notasValidas.length;
+
+        console.log(`Atleta: ${nome}`);
+        console.log(`Notas Obtidas: ${notas}`);
+        console.log(`Média Válida: ${media}`);
+        console.log("");
+    }
+}
+
+calcularMedia(atletas);
